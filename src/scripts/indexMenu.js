936f7b2c61b4d4db601fb7efdbbd627187a1b6a0
INTJcodes <lolarosegooch@gmail.com>
2025-09-06 11:02:16 +0100

    Changing the design of the website a bit

--- src/scripts/indexMenu.js
+++ src/scripts/indexMenu.js
@@ -1,18 +1,3 @@
-const nav = document.querySelector('header');
-const landing = document.getElementById("landing");
-
-function setLandingDistance() {
-	let height = window.getComputedStyle(nav).getPropertyValue("height").slice(0, -2);
-	let landingmargin = window.getComputedStyle(landing).getPropertyValue("margin-bottom").slice(0, -2);
-	landing.style.marginTop = Number(landingmargin) + Number(height) + "px";
-};
-
-setLandingDistance();
-			
-window.addEventListener("resize", () => {
-	setLandingDistance();
-});
-
 document.querySelector('.hamburger')?.addEventListener('click', () => {
 	document.querySelector('.navigation')?.classList.toggle('visible');
 	setLandingDistance();
